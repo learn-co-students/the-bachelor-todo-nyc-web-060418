78141b55c3d651fd88b009eeea96e4a39fb939a4
github username <github email address>
2018-05-26 15:31:47 -0700

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,62 @@
+# Bachelor Nested Iteration Lab
+# Laura Nadolski
+# May 26 2018
+
+# Returns the first name of the given season's winner.
 def get_first_name_of_season_winner(data, season)
-  # code here
+  season_hash_array = data[season]
+  season_winner = ""
+  season_hash_array.each do |contestant|
+    if contestant["status"] == "Winner"
+      season_winner = contestant["name"]
+    end
+  end
+  season_winner = season_winner.split
+  return season_winner[0]
 end
 
+# Returns name of woman with matching occupation.
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |season, contestant_data|
+    contestant_data.each do |contestant|
+      if contestant["occupation"] == occupation
+        return contestant["name"]
+      end
+    end
+  end
 end
 
+# Returns a counter of the no. of contestants from the given hometown.
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  counter = 0
+  data.each do |season, contestant_data|
+    contestant_data.each do |contestant|
+      if contestant["hometown"] == hometown
+        counter += 1
+      end
+    end
+  end
+  return counter
 end
 
+# Returns the occupation of the first contestant from the specified hometown.
 def get_occupation(data, hometown)
-  # code here
+  data.each do |season, contestant_data|
+    contestant_data.each do |contestant|
+      if contestant["hometown"] == hometown
+        return contestant["occupation"]
+      end
+    end
+  end
 end
 
+# Calculates the average age of all contestants for the given season.
 def get_average_age_for_season(data, season)
-  # code here
+  season_hash_array = data[season]
+  counter = 0
+  season_hash_array.each do |contestant|
+    counter += contestant["age"].to_f
+  end
+  counter = counter / season_hash_array.length
+  return counter.round
 end
